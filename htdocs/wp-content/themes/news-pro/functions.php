6129d0c268acd6c39d7d212f592b05933b1c89ad
SergeyPek <sergeypekar77@gmail.com>
2016-04-27 11:13:17 +0300

    make GA async, plugin updates

--- htdocs/wp-content/themes/news-pro/functions.php
+++ htdocs/wp-content/themes/news-pro/functions.php
@@ -372,10 +372,7 @@ add_action('genesis_before', 'add_google_analytics', 6);
 
 function add_google_analytics(){
 	?><script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 		ga('create', 'UA-76802967-1', 'auto', 'slideshowTracker');
 		ga('create', 'UA-75246817-1', 'auto');
 	<?php
@@ -384,7 +381,9 @@ function add_google_analytics(){
 		ga('slideshowTracker.send', 'pageview');
 	<?php else: ?>
 		ga('send', 'pageview');
-	<?php endif; ?></script><?php
+	<?php endif; ?></script>
+	<script async src='https://www.google-analytics.com/analytics.js'></script>
+	<?php
 }
 
 function itv_get_primary_category($post = null){
